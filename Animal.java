788b4c5a4e9f916b38658cf4aa5f353c32948a5f
FrenchFries2424 <111604571+FrenchFries2424@users.noreply.github.com>
2024-02-07 16:09:26 -0600

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,16 @@
+public interface Animal {
+	int getAge();
+	int getCost();
+	int getNumBabies();
+	int getBaseFoodCost();
+	double getPayOff();
+
+
+
+	void setAge(int age);
+	void setCost(int cost);
+	void setNumBabies(int numBabies);
+	void setBaseFoodCost(int baseFoodCost);
+	void setPayOff(double payOff);
+
+}
