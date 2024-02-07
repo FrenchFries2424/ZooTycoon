2f15cd9b6353b2221be248af75309e15bb8d9eac
FrenchFries2424 <111604571+FrenchFries2424@users.noreply.github.com>
2024-02-07 16:10:15 -0600

    Create Penguin.java

---
+++ Penguin.java
@@ -0,0 +1,67 @@
+
+public class Penguin implements Animal{
+	private int age;
+	private int cost;
+	private int numOfBabies;
+	private int foodCost;
+	private double payoff;
+	
+	public Penguin(int a, int b, int c, int d, double e) {
+		age = a;
+		cost = b;
+		numOfBabies =c;
+		foodCost = d;
+		payoff = e;
+	}
+public int getAge() {
+		
+		return age;
+	}
+	@Override
+	public int getCost() {
+		
+		return cost;
+	}
+	@Override
+	public int getNumBabies() {
+		
+		return numOfBabies;
+	}
+	@Override
+	public int getBaseFoodCost() {
+		
+		return foodCost;
+	}
+	@Override
+	public double getPayOff() {
+		
+		return payoff;
+	}
+	@Override
+	public void setAge(int age) {
+		this.age = age;
+		
+	}
+	@Override
+	public void setCost(int cost) {
+		this.cost = cost;
+		
+	}
+	@Override
+	public void setNumBabies(int numBabies) {
+		this.numOfBabies = numBabies;
+		
+	}
+	@Override
+	public void setBaseFoodCost(int baseFoodCost) {
+		this.foodCost = baseFoodCost;
+		
+	}
+	@Override
+	public void setPayOff(double payOff) {
+		this.payoff = payOff;
+		
+	}
+	
+}
+
